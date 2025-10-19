219687b8f7d791045a3618fc54e949410ee106fd
уля <wx33wxiw0@gmail.com>
2025-10-19 17:42:33 +0300

    updated bot.py

--- bot.py
+++ bot.py
@@ -45,4 +45,10 @@ def ban_bad_words(message):
     except:
         pass
 
+@bot.message_handler(content_types=['new_chat_members'])
+def make_some(message):
+    bot.send_message(message.chat.id, 'I accepted a new user!')
+    bot.approve_chat_join_request(message.chat.id, message.from_user.id)
+
 bot.infinity_polling(none_stop=True)
+
