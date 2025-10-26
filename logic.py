fdd1a27e58a23be59b0822db63383c8ed59f4495
уля <wx33wxiw0@gmail.com>
2025-10-26 19:27:24 +0300

    Add files via upload
    pokemon UPD

---
+++ logic.py
@@ -0,0 +1,97 @@
+from random import randint
+import requests
+
+class Pokemon:
+    pokemons = {}
+    # Инициализация объекта (конструктор)
+    def __init__(self, pokemon_trainer):
+
+        self.pokemon_trainer = pokemon_trainer   
+
+        self.pokemon_number = randint(1,1000)
+        self.img = self.get_img()
+        self.name = self.get_name()
+        self.hp = randint(50,100)
+        self.power = randint(50,100)
+        self.hunger = 0
+
+
+        Pokemon.pokemons[pokemon_trainer] = self
+
+    # Метод для получения картинки покемона через API
+    def get_img(self):
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return (data['sprites']["other"]["official-artwork"]["front_default"])
+        else:
+            return "https://upload.wikimedia.org/wikipedia/ru/7/77/Pikachu.png"
+    
+    # Метод для получения имени покемона через API
+    def get_name(self):
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return (data['forms'][0]['name'])
+        else:
+            return "Pikachu"
+
+
+    # Метод класса для получения информации
+    def info(self):
+        return f"Имя твоего покеомона: {self.name}, HP {self.hp}, сила {self.power}"
+
+    # Метод класса для получения картинки покемона
+    def show_img(self):
+        return self.img
+
+    def attack(self, enemy):
+        if isinstance(enemy, Wizard): # Проверка на то, что enemy является типом данных Wizard (является экземпляром класса Волшебник)
+            chance = randint(1,5)
+            if chance == 1:
+                return 
+        if enemy.hp > self.power:
+            enemy.hp -= self.power
+            return f"Сражение @{self.pokemon_trainer} с @{enemy.pokemon_trainer}"
+        else:
+            enemy.hp = 0
+            return f"Победа @{self.pokemon_trainer} над @{enemy.pokemon_trainer}! "
+    def feed(self):
+    
+        if self.hunger > 0:
+            self.hunger -= 1
+            return f"{self.name} покушал!"
+        else:
+            return f"{self.name} не голоден!"
+
+    def is_rare(self):
+   
+        rare_numbers = [150, 151, 249, 250, 251]
+        return self.pokemon_number in rare_numbers
+
+
+
+class Fighter(Pokemon):
+    def attack(self, enemy):
+        super_power = randint(5,15)
+        self.power += super_power
+        result = super().attack(enemy)
+        self.power -= super_power
+        return result + f"\nБоец применил супер-атаку силой:{super_power} "
+
+    def info(self):
+        return "У вас покемон - боец\n" + super().info()
+    
+class Wizard(Pokemon):
+    pass
+if __name__ == '__main__':
+    wizard = Wizard("username1")
+    fighter = Fighter("username2")
+
+    print(wizard.info())
+    print()
+    print(fighter.info())
+    print()
+    print(fighter.attack(wizard))
